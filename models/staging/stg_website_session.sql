af1318c030714d87c036db92c16a2817d3e28e0b
Sebastian Curea <sebastiancurea@outlook.com>
2026-05-08 10:11:39 +0000

    created stg tables

--- models/staging/stg_website_session.sql
+++ models/staging/stg_website_session.sql
@@ -5,8 +5,9 @@ WITH SOURCE AS (
 SELECT
     CAST(website_session_id AS STRING) AS session_id,
     CAST(created_at AS TIMESTAMP) AS session_at,
+    CAST(created_at AS DATE) AS ordered_date, -- New column for easy daily reporting
     CAST(user_id AS STRING) AS user_id,
-    CAST(utm_source AS STRING) AS utm_sourSTRING
+    CAST(utm_source AS STRING) AS utm_source,
     CAST(utm_campaign AS STRING) AS utm_campaign,
     CAST(utm_content AS STRING) AS utm_content,
     CAST(device_type AS STRING) AS device_type,
