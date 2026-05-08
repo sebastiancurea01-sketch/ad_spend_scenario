8076d3def1149549973fcd73409090500b02cf53
Sebastian Curea <sebastiancurea@outlook.com>
2026-05-08 09:59:41 +0000

    Source definition

---
+++ models/staging/stg_website_session.sql
@@ -0,0 +1,14 @@
+WITH SOURCE AS (
+    SELECT * FROM {{ source('internal_data', 'website_session') }}
+)
+
+SELECT
+    CAST(website_session_id AS STRING) AS session_id,
+    CAST(created_at AS TIMESTAMP) AS session_at,
+    CAST(user_id AS STRING) AS user_id,
+    CAST(utm_source AS STRING) AS utm_sourSTRING
+    CAST(utm_campaign AS STRING) AS utm_campaign,
+    CAST(utm_content AS STRING) AS utm_content,
+    CAST(device_type AS STRING) AS device_type,
+    CAST(http_referer AS STRING) AS http_referer
+FROM SOURCE
